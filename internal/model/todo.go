f1a794a7d4903e926545f86e4616410186db5737
Ujjwal Mahar <ujjwal.mahar01@gmail.com>
2024-05-10 12:03:00 +0530

    created todo struct

--- internal/model/todo.go
+++ internal/model/todo.go
@@ -2,3 +2,11 @@
 This file contains Todo struct definition. It represents the data structure of a todo item.
 */
 
+package model
+
+type Todo struct {
+	ID int `json:"id"`
+	Title string `json:"title"`
+	Description string `json:"description"`
+	Completed bool `json:"completed"`
+}
